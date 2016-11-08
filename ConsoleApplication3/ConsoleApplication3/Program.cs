3f63774a84ec3c69c2c2d984d7647e755b77a0a4
a15995 <123@123.dk>
2016-11-08 13:25:47 +0100

    Til og med opgave5

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Hello.World
+{
+    public class HelloWorld
+    {
+        public static void Main()
+        {
+            //Opgave1();
+            //Opgave2();
+            //Opgave3();
+            //Opgave4();
+            Opgave5();
+        }
+        public static void Opgave1()
+        {
+            //Her er opgave 1
+            Debug.WriteLine("Hello");
+            Debug.WriteLine("Søren Larsen");
+        }
+        public static void Opgave2()
+        {
+            //her er opgave 2
+            int x = 12 + 13;
+            Debug.WriteLine(x);
+        }
+        public static void Opgave3()
+        {
+            //her er opgave 3
+            double a = 24;
+            double b = 5;
+            double x = a / b;
+            Debug.WriteLine(x);
+        }
+        public static void Opgave4()
+        {
+            //her er opgave 4
+            int a = -1 + 3 * 5;
+            int b = (24 + 5) % 7;
+            int c = 15 + -4 * 6 / 11;
+            int d = 2 + 10 / 6 * 1 - 7 % 2;
+            Debug.WriteLine(a + " " + b + " " + c + " "+ d);
+        }
+        public static void Opgave5()
+        {
+            Console.WriteLine("Indtast første tal: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Indtast andet tal: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a*b);
+            Console.ReadKey();
+        }
+    }
+}
